b367ea2f932029fda2bc4b7f5aca1ec598b2ed22
Cole Peters <cole@colepeters.com>
2022-07-30 16:53:49 -0500

    Fix untracked changes due to file casing change

---
+++ components/MultiSelect/index.js
@@ -0,0 +1,19 @@
+import MultiSelectButtonItem from './MultiSelectButtonItem'
+import MultiSelectCheckboxItem from './MultiSelectCheckboxItem'
+import MultiSelectGroup from './MultiSelectGroup'
+import MultiSelectRoot from './MultiSelectRoot'
+import MultiSelectSearchItem from './MultiSelectSearchItem'
+import MultiSelectSelectAll from './MultiSelectSelectAll'
+import MultiSelectSeparator from './MultiSelectSeparator'
+
+const MultiSelect = {
+  Root: MultiSelectRoot,
+  Group: MultiSelectGroup,
+  CheckboxItem: MultiSelectCheckboxItem,
+  SearchItem: MultiSelectSearchItem,
+  Separator: MultiSelectSeparator,
+  ButtonItem: MultiSelectButtonItem,
+  SelectAll: MultiSelectSelectAll,
+}
+
+export default MultiSelect
